dbffabe5807226fad90fdfb2ea815024ad4a2e48
superimpega <awesomealexmeh1@hotmail.co.uk>
2013-07-23 22:35:47 +0100

    Update s.js

--- s.js
+++ s.js
@@ -1,21 +1,32 @@
-API.sendChat('/me Running Hue Kicker version 1.1')
+API.sendChat('/me Running Hue Kicker version 1.2')
     API.on (API.CHAT, function (data) {
       console.log(data);
              var a = data.message.toLowerCase();
               if (a.indexOf('hue') > -1)
               {
-              	API.moderateKickUser(data.fromID, "spamming");
+              	API.moderateKickUser(data.fromID, "Spamming hue");
               }
               else if (a.indexOf('br') > -1)
               {
-              	API.moderateKickUser(data.fromID, "spamming");
+              	API.moderateKickUser(data.fromID, "Spamming br");
               }
               else if (a.indexOf('hu3') > -1)
               {
-              	API.moderateKickUser(data.fromID, "spamming");
+              	API.moderateKickUser(data.fromID, "Spamming hu3");
               }
               else if (a.indexOf('kkkkkkk') > -1)
               {
-                API.moderateKickUser(data.fromID, "spamming");
+                API.moderateKickUser(data.fromID, "Spamming k");
               }
+              else if (a.indexOf('zuera') > -1)
+              {
+                API.moderateKickUser(data.fromID, "Spamming zuera");
+              {
+              else if (a.indexOf('spoder mafia') > -1)
+              {
+                API.moderateKickUser(data.fromID, "Spamming spoder mafia");
+              {
+              else if (a.indexOf('gibe moni plox') > -1)
+              {
+                API.moderateKickUser(data.fromID, "Spamming");
           })
